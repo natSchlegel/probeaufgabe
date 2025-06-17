08fbae51a6d9995e7d0f0ca060b6a8ac1f60e3ee
Natascha <46815651+natSchlegel@users.noreply.github.com>
2025-06-17 13:17:13 +0200

    remove logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Logo from "../public/logo.svg";
+// import Logo from "../public/logo.svg";
 
 const Header = () => {
   return (
     <header className="h-1/5 ml-32">
-        <Logo className="w-32 mt-10 h-auto" />
+        {/* <Logo className="w-32 mt-10 h-auto" /> */}
     </header>
   );
 };

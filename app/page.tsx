ac3bf4c32c3ac3a9b449c1492e1b4d19fdc1f15f
Natascha <46815651+natSchlegel@users.noreply.github.com>
2025-06-19 16:42:43 +0200

    Merge branch 'main' of https://github.com/natSchlegel/probeaufgabe into prod

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Footer from "../components/Footer";
 import Main from "../components/Main";
 import Header from "../components/Header";
-import { TrackVisitor } from "../components/TrackVisitor";
+import { prisma } from '@/lib/prisma';
 
 // Home page component
 export default async function Home() {
   // Track the visitor
-  await TrackVisitor();
+  await prisma.visitor.create({});
+
   return (
     <>
       <Header />
